e10a523fcb7fd1f7eb82cfb98d6e5214b33ee961
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-08 09:09:46 +0100

    10-Clase Funciones - P1

---
+++ Servicio/system/Funciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicio
+{
+    public class Funciones
+    {
+        public static void Logs(string nombre_archivo, string descripcion)
+        {
+            string directorio = AppDomain.CurrentDomain.BaseDirectory + "logs/" +
+                DateTime.Now.Year.ToString() + "/" +
+                DateTime.Now.Month.ToString() + "/" +
+                DateTime.Now.Day.ToString();
+
+        }
+    }
+}
